9743c98fdb7aa13c8e07fca6b161893e5a7c9e46
Rehan Chalana <rehanchalanaprsnl@gmail.com>
2023-09-23 13:54:46 +0530

    Added javascript

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+
+// arsh script
+
+
 var finalExpense = '';
+Balance = 100000.00 ;
+// let expenseHTML = JSON.parse(localStorage.getItem('history'))|| [];
+history();
 
 // JSON.parse(localStorage.getItem('finalExpense'))||
 
@@ -8,6 +15,11 @@ function findInput() {
     saveCalculation();
     return expense;
 };
+
+function changeBalance() {
+    Balance = eval(Balance + '-' + findInput());
+    document.querySelector('.blcAmount').innerText = Balance;
+}
 
 function addExpense() {
     const newexpense = findInput();
@@ -23,6 +35,50 @@ function showAnswer() {
     saveCalculation();
 };
 
+// function saveCalculation(){
+//     localStorage.setItem('finalExpense',finalExpense);
+//     localStorage.setItem('Balance',Balance);
+//     localStorage.setItem('expenseHTML',expenseHTML);
+// };
+
+// let expenseHTML = JSON.parse(localStorage.getItem('history'))|| [];
+function history() {
+    const title = document.querySelector('.expenseTitle').value;
+    const category = document.querySelector('.category').value;
+    const expense = document.querySelector('.expenseInput').value;
+    const date = document.querySelector('.date').value;
+
+    const html = `
+
+                <div id="entry">
+                    <div class="entryContent">
+                        <div class="rightEntry">
+                        <h3 id="entryTitle"> ${title} </h3>
+                        <h4 id="entryCategory"> ${category} </h4>
+                        </div>
+
+                        <div class="leftEntry">
+                        
+                        <h3 id="entryAmount">Rs ${expense}</h3>
+                        <p id="entryDate">${date}</p>
+                        
+                        
+                        </div>
+                        
+                    </div>
+                </div>
+                `;
+
+        expenseHTML = html;
+    
+
+    document.querySelector('.history')
+        .innerHTML += expenseHTML;
+    saveCalculation();
+}
+
 function saveCalculation(){
     localStorage.setItem('finalExpense',finalExpense);
+    localStorage.setItem('Balance',Balance);
+    localStorage.setItem('expenseHTML',expenseHTML);
 };
