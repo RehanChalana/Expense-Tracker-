4511112cc8eeaee57b21c4c9e47fd2caeaf95773
Rehan Chalana <rehanchalanaprsnl@gmail.com>
2023-10-25 22:50:03 +0530

    changed the analysis.html layout

--- analysis.js
+++ analysis.js
@@ -1,51 +1,34 @@
-const catChart = document.getElementById('catChart');
+const labels = ["Jan","Feb","june","july","Aug","May","Nov"]
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: '#720e9e',
+    borderWidth: 1
+  }]
+};
 
-var data1={
-    labels: ["Clothing","Snacks","Food","Education","Subscriptions","Bills"],
-    datasets: [
-        {
-            label: "Expense Categories",
-            data: [10,20,30,50,60,10],
-            backgroundColor:[
-                "#FAEBD7",
-                "#DCDCDC",
-                "#E9967A",
-                "#F5DEB3",
-                "#9ACD32"
-            ],
-            borderColor: [
-                "#E9DAC6",
-                "#CBCBCB",
-                "#D88569",
-                "#E4CDA2",
-                "#89BC21"
-              ],
+let barGraphdiv = document.getElementById("barGraph").getContext("2d");
+
+var barGraph = new Chart(barGraphdiv, {
+  type: 'bar',
+  data: data,
+  options: {
+     responsive: false,
+     scales:{
+      x:{
+        grid:{
+          color:'white'
+        },
+        
+      },
+
+      y:{
+        grid:{
+          color:'white'
         }
-    ]
-};
-
-
-var options = {
-    responsive: true,
-    title: {
-      display: true,
-      position: "top",
-      text: "Doughnut Chart",
-      fontSize: 18,
-      fontColor: "#111"
-    },
-    legend: {
-      display: true,
-      position: "bottom",
-      labels: {
-        fontColor: "#333",
-        fontSize: 16
       }
-    }
-};
-
-new Chart(catChart,{
-    type : "doughnut",
-    data : data1,
-    options:options,   
+     }
+  },
 });
