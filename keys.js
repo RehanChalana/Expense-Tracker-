a7ab07d2648d2aa0cf2e5b34bd8eb0d3b01504e8
Rehan Chalana <rehanchalanaprsnl@gmail.com>
2023-11-01 18:31:58 +0530

    added supabase support and login page checks users

---
+++ keys.js
@@ -0,0 +1,3 @@
+
+export const supabase_url = 'https://lbupusrzdvasyetdvxgk.supabase.co';
+export const supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxidXB1c3J6ZHZhc3lldGR2eGdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg0OTQ3MjIsImV4cCI6MjAxNDA3MDcyMn0.fbusSsusVDqx_mGGKfQaqnvhvtI0ouMVgAgQBFPkS0k';
---
+++ login.js
@@ -0,0 +1,42 @@
+import {supabase_url,supabase_key} from './keys.js';
+const database = supabase.createClient(supabase_url,supabase_key);  
+const getUserDetails =  await database.from('users').select('walletname');
+// console.log(getUserDetails.data[]["walletname"]);
+let wallet_name;
+let password_in;
+let isUser = false;
+document.querySelector(".submitButton").addEventListener("click", checkLogin);
+
+
+async function checkLogin(){
+    wallet_name = document.querySelector("#walletNameIn").value;
+    password_in = document.querySelector("#passwordIn").value;
+    for(let i=0; i<getUserDetails.data.length;i++){
+        if(wallet_name.localeCompare(getUserDetails.data[i]["walletname"])==0){
+            isUser = true;   
+            break;
+    }}
+    if(isUser){
+        const getPassDetails = await database.from('users').select('password').eq("walletname",wallet_name);
+        if(password_in.localeCompare(getPassDetails.data[0]["password"])==0){
+            alert("LOGIN SUCCESS");
+        } else{
+            alert("wrong password");
+        }
+    } else{
+        alert("User not found please signup first")
+    }
+}
+
+
+
+
+
+
+   
+
+
+
+   
+
+
