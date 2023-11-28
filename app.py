d90ed49b80f11a290544433f49bcbc3183baf7f3
RehanChalana <rehanchalanaprsnl@gmail.com>
2023-11-28 18:55:17 +0530

    formatted the pdf file with correct distances

--- app.py
+++ app.py
@@ -14,19 +14,36 @@ def generate_pdf(data):
     c = canvas.Canvas(pdf_filename)
 
     # Set font and font size
-    c.setFont("Helvetica", 12)
+   
 
     # Add a title
-    c.drawString(100, 750, "Expense Report")
+    # c.drawString(100, 750, "Expense Report")
     
 
     # Add expenses to the PDF
+    y=750
+    c.setFont("Helvetica",25)
+    
+    c.drawString(230,800,f"Expense Report!")
+    
+    c.drawString(90,750,f"Title")
+    c.drawString(240,750,f"Amount")
+    c.drawString(340,750,f"Date")
+    c.drawString(440,750,f"Category")
+    c.setFont("Helvetica", 12)
+    
     y_position = 730  # Initial Y position for expenses
     # x=100
     for i in data:
-        amount = i
+        Title = i["Title"]
+        amount = i["Amount"]
+        Date = i["Date"]
+        category = i["category"]
         # Add expense details to the PDF
-        c.drawString(16,y_position, f"${amount}")
+        c.drawString(100,y_position, f"{Title}")
+        c.drawString(250,y_position,f"{amount}")
+        c.drawString(350,y_position,f"{Date}")
+        c.drawString(450,y_position,f"{category}")
         y_position -= 20  # Move to the next line
         # x+=10
 
