28c78fb525d98e6415b95c0729ecdb408f4c6285
RehanChalana <rehanchalanaprsnl@gmail.com>
2023-12-02 16:05:56 +0530

    changed the whole design of history.html

--- history.js
+++ history.js
@@ -5,32 +5,46 @@ console.log(getUserDetails.data[0]);
 let user_id = sessionStorage.getItem("user_id");
 let historyData = await database.from("ExpenseHistory").select("Amount,Date,category,Title").eq("user_id",user_id);
 
+let historyTable = document.querySelector("#historyTable");
+
 for(let i=0;i<historyData.data.length;i++){
-    let tempSrDiv = document.createElement('div');
+    let tempRow = document.createElement('div');
+    tempRow.classList.add("historyRow");
+    let tempLeft = document.createElement('div');
+    tempLeft.classList.add('historyLeft');
+    let tempTextBox = document.createElement('div');
+    tempTextBox.classList.add('historyText');
+    let tempRight = document.createElement('div');
+    tempRight.classList.add('historyRight');
+    
     let tempTitleDiv = document.createElement('div');
     let tempAmountDiv = document.createElement('div');
     let tempCategoryDiv = document.createElement('div');
     let tempDateDiv = document.createElement('div');
-    tempSrDiv.innerHTML = `<div class="historycolElement">${i+1+"."}</div>` 
-    tempTitleDiv.innerHTML = `<div class="historycolElement">${historyData.data[i]['Title']}</div>`
-    tempAmountDiv.innerHTML = `<div class="historycolElement">${'Rs '+historyData.data[i]['Amount']}</div>`
-    tempCategoryDiv.innerHTML = `<div class="historycolElement">${historyData.data[i]['category']}</div>`
-    tempDateDiv.innerHTML = `<div class="historycolElement">${historyData.data[i]['Date']}</div>`
+    tempTitleDiv.innerHTML = `<div class="historyTitle">${historyData.data[i]['Title']}</div>`
+    tempAmountDiv.innerHTML = `<div class="historyAmount">${'Rs '+historyData.data[i]['Amount']}</div>`
+    tempCategoryDiv.innerHTML = `<div class="category">${historyData.data[i]['category']}</div>`
+    tempDateDiv.innerHTML = `<div class="historyDate">${historyData.data[i]['Date']}</div>`
     tempAmountDiv.style.color='#D22B2B';
     tempTitleDiv.style.color='#9932cc';
-    document.querySelector('#serialNumber').appendChild(tempSrDiv);
-    document.querySelector('#transTitle').appendChild(tempTitleDiv);
-    document.querySelector('#transCategory').appendChild(tempCategoryDiv);
-    document.querySelector('#transAmount').appendChild(tempAmountDiv);
-    document.querySelector('#transDate').appendChild(tempDateDiv);
-    tempTitleDiv.style.padding='2px 20px 2px 47px';
-  
-    if(i%2!=0){
-      tempTitleDiv.style.backgroundColor='#1b1b1b';
-      tempAmountDiv.style.backgroundColor='#1b1b1b';
-      tempSrDiv.style.backgroundColor='#1b1b1b';
-      tempCategoryDiv.style.backgroundColor='#1b1b1b';
-      tempDateDiv.style.backgroundColor='#1b1b1b';
-    }
+
+   
+
+    tempTextBox.appendChild(tempTitleDiv);
+    tempTextBox.appendChild(tempCategoryDiv);
+    let tempIcon = document.createElement('i');
+    tempIcon.classList.add('fa-solid','fa-user','historyIcon');
+    tempLeft.appendChild(tempIcon);
+    tempLeft.appendChild(tempTextBox);
+    tempRow.appendChild(tempLeft);
+    historyTable.appendChild(tempRow);
+
+
+    tempRight.appendChild(tempAmountDiv);
+    tempRight.appendChild(tempDateDiv);
+    tempRow.appendChild(tempRight);
+    
+    
+
   }
   
