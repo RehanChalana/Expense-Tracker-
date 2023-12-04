bf740e90b63e7eb45fba3432f36f7a8d3d3ad2c2
RehanChalana <rehanchalanaprsnl@gmail.com>
2023-12-05 01:49:04 +0530

    limit export works fine

--- analysis.js
+++ analysis.js
@@ -276,35 +276,5 @@ const categoryGraph = new Chart(categoryGraphDiv,{
   }
 })
 
-// trying to update history column
-for(let i=0;i<historyData.data.length;i++){
-  let tempSrDiv = document.createElement('div');
-  let tempTitleDiv = document.createElement('div');
-  let tempAmountDiv = document.createElement('div');
-  let tempCategoryDiv = document.createElement('div');
-  let tempDateDiv = document.createElement('div');
-  tempSrDiv.innerHTML = `<div class="historycolElement">${i+1+"."}</div>` 
-  tempTitleDiv.innerHTML = `<div class="historycolElement">${historyData.data[i]['Title']}</div>`
-  tempAmountDiv.innerHTML = `<div class="historycolElement">${'Rs '+historyData.data[i]['Amount']}</div>`
-  tempCategoryDiv.innerHTML = `<div class="historycolElement">${historyData.data[i]['category']}</div>`
-  tempDateDiv.innerHTML = `<div class="historycolElement">${historyData.data[i]['Date']}</div>`
-  tempAmountDiv.style.color='#D22B2B';
-  tempTitleDiv.style.color='#9932cc';
-  document.querySelector('#serialNumber').appendChild(tempSrDiv);
-  document.querySelector('#transTitle').appendChild(tempTitleDiv);
-  document.querySelector('#transCategory').appendChild(tempCategoryDiv);
-  document.querySelector('#transAmount').appendChild(tempAmountDiv);
-  document.querySelector('#transDate').appendChild(tempDateDiv);
-  tempTitleDiv.style.padding='2px 20px 2px 47px';
-
-  if(i%2!=0){
-    tempTitleDiv.style.backgroundColor='#1b1b1b';
-    tempAmountDiv.style.backgroundColor='#1b1b1b';
-    tempSrDiv.style.backgroundColor='#1b1b1b';
-    tempCategoryDiv.style.backgroundColor='#1b1b1b';
-    tempDateDiv.style.backgroundColor='#1b1b1b';
-  }
-}
-
 
 // making rows hoverable 
--- sidenav.js
+++ sidenav.js
@@ -2,7 +2,7 @@ import {supabase_url,supabase_key} from './keys.js';
 const database = supabase.createClient(supabase_url,supabase_key);  
 const username = sessionStorage.getItem("user");
 let user_id = sessionStorage.getItem("user_id");
-let userData = await database.from("users").select("Budget,userBalance").eq("user_id",user_id);
+let userData = await database.from("users").select("Budget,userBalance,password").eq("user_id",user_id);
 
 document.querySelector("#endSideBar").addEventListener("click",opennav);
 document.querySelector(".closenav").addEventListener("click",closenav);
@@ -39,6 +39,47 @@ async function changeBudget(){
         }   
     }
 }
+
+// making change password working!
+
+let changePasswordBtn = document.querySelector('#changePasswordBtn');
+changePasswordBtn.addEventListener("click",changePassword);
+
+async function changePassword(){
+    let password = document.querySelector("#curPassword").value;
+    let newPassword = document.querySelector("#newPassword").value;
+    let confirmPassword = document.querySelector("#confirmPassword").value;
+
+    if(password.localeCompare(userData.data[0]["password"])!=0){
+        alert("please enter the correct current password")
+        return;
+    } else{
+        if(newPassword.localeCompare(confirmPassword)!=0){
+            alert("new password and confirm password does not match!")
+            return;
+        } else{
+            let passwordExport = await database.from("users").update([{"password":newPassword}]).eq("user_id",user_id);
+            alert("password changed successfuly");
+            window.location.href="index.html";
+        }
+    }
+
+}
+
+document.querySelector("#submitLimitData").addEventListener("click",changeLimits);
+
+async function changeLimits(){
+    let dailyIn = document.querySelector("#dailyIn").value;
+    let monthlyIn = document.querySelector("#monthlyIn").value;
+    try{
+        let limitExport = await database.from("users").update([{"dailyLimit":dailyIn,"monthlyLimit":monthlyIn}]).eq("user_id",user_id);
+        alert("Limits updated succesfully!")
+    } catch{
+        alert("Error 404 signal not found")
+    }
+
+}
+
 
 
 
